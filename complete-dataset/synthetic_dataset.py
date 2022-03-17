c96031fa86f49281ddb51859407600aca9bcae1c
ims43 <ims43@tamu.edu>
2022-03-17 18:54:46 -0500

    skeleton functions for image alterations

--- complete-dataset/synthetic_dataset.py
+++ complete-dataset/synthetic_dataset.py
@@ -1,1 +1,24 @@
-import PIL
+from PIL import Image
+import os
+
+imageDir = os.path.join(os.getcwd(), 'images')
+
+
+# adjust contrast from 0.25 to 0.9 at intervals of 0.05
+def applyContrast(directory):
+    pass
+
+
+# adjust color balance from 0.0 to 0.9 at intervals of 0.1
+def applyColor(directory):
+    pass
+
+
+# adjust brightness from 0.3 to 0.9 at intervals of 0.05
+def applyBrightness(directory):
+    pass
+
+
+# adjust sharpness from 0.25 to 0.9 at intervals of 0.05
+def applySharpness(directory):
+    pass
