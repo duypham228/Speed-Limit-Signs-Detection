f49b6516f1257264d07a52fefee7561a6ed619e7
ims43 <ims43@tamu.edu>
2022-03-17 19:16:00 -0500

    Built initial function to apply color change

--- complete-dataset/synthetic_dataset.py
+++ complete-dataset/synthetic_dataset.py
@@ -17,7 +17,13 @@ def applyContrast(directory):
 
 # adjust color balance from 0.0 to 0.9 at intervals of 0.1
 def applyColor(directory):
-    pass
+    for image in directory:
+        img = Image.open(image)
+        filename = img.filename
+        enhancer = ImageEnhance.Color(img)
+        for i in range(9, 90, 10):
+            adjustVal = i / 100
+            enhancer.enhance(adjustVal).save(os.path.join(directory, filename + 'color' + str(i)))
 
 
 # adjust brightness from 0.3 to 0.9 at intervals of 0.05
