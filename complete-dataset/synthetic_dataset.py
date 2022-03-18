8cabcd747fa052a7c7d4a34faaca2b9efa0b65b3
ims43 <ims43@tamu.edu>
2022-03-17 19:13:10 -0500

    Initial function to apply contrast to images

--- complete-dataset/synthetic_dataset.py
+++ complete-dataset/synthetic_dataset.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 
 imageDir = os.path.join(os.getcwd(), 'images')
@@ -6,7 +6,13 @@ imageDir = os.path.join(os.getcwd(), 'images')
 
 # adjust contrast from 0.25 to 0.9 at intervals of 0.05
 def applyContrast(directory):
-    pass
+    for image in directory:
+        img = Image.open(image)
+        filename = img.filename
+        enhancer = ImageEnhance.Contrast(img)
+        for i in range(25, 90, 5):
+            adjustVal = i / 100
+            enhancer.enhance(adjustVal).save(os.path.join(directory, filename + 'contrast' + str(i)))
 
 
 # adjust color balance from 0.0 to 0.9 at intervals of 0.1
