ba1795529f24ca65cf7b9373f7778ffa0e097b01
sms0325 <sms0325@tamu.edu>
2022-04-04 09:59:29 -0500

    Created model saving method, and more

---
+++ model.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow import keras
+import ssl
+import requests
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import os
+from PIL import Image
+
+from sklearn.metrics import confusion_matrix
+
+# class Model():
+    
+    # def dataset_split(self):
