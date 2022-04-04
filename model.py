cb3c9d60f90edcb04453f0bcec3d9644712ed6dd
sms0325 <sms0325@tamu.edu>
2022-04-04 10:42:45 -0500

    Model import function, all images in this branch

--- model.py
+++ model.py
@@ -13,6 +13,24 @@ from PIL import Image
 
 from sklearn.metrics import confusion_matrix
 
-# class Model():
+def import_model():
+    new_model = tf.keras.models.load_model('saved_model/my_model')
+
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    # Evaluate the restored model
+    loss, acc = new_model.evaluate(x_test, y_test, verbose=2)
+
+    print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
+    print(new_model.predict(x_test).shape)
     
-    # def dataset_split(self):
+    return new_model
+
+def main():
+    model = import_model()
+
+if __name__ == '__main__':
+    main()
