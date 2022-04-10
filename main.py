2a517f81ca4eb557567e10157b3e7360107b0d58
sms0325 <sms0325@tamu.edu>
2022-04-10 15:37:56 -0500

    Further model clean-up testing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy
+from dev_ws.model import import_model
 
 
 # Mapping function to reset 8 bits to 2 bits scale
@@ -28,13 +29,13 @@ new_height = 300
 dsize = (new_width, new_height)
 resize_img = cv2.resize(img, dsize)
 bottom_half = resize_img[150:300, :]
-# # print("after: ", resize_img.shape)
+# print("after: ", resize_img.shape)
 
 # print(len(resize_img[:,10]))
 # print("Before: ", img.shape)
 
 # current size img[150h, 250w]
-os.chdir(testpath)
+# os.chdir(testpath)
 # Convert image into gray scale
 leftBound = 15
 rightBound = 235
@@ -43,9 +44,9 @@ crop = bottom_half[:bottomBound,leftBound:rightBound]
 gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
 # print(len(gray[0]))
 
-blackWhite = numpy.array(list(map(mapping_helper, gray)))
+blackWhite = numpy.array(list(map(mapping_helper, gray)), dtype="uint8")
 # print(list(blackWhite))
-cv2.imwrite("Test.jpeg",blackWhite)
+# cv2.imwrite("Test.jpeg",blackWhite)
 
 # print(gray[:, 20])
 # Dense Threshold: 10%
@@ -61,10 +62,27 @@ for i in range(0, len(blackWhite[0]), portion):
     if i > portion * 2 and i < portion * 10:
         partDict[i] = blackRatio
     name = "test" + str(i) + ".jpeg"
-    cv2.imwrite(name ,part)
+    # cv2.imwrite(name ,part)
 
 index = min(partDict, key=partDict.get)
 firstDigit = blackWhite[:,0:index+10]
 secondDigit = blackWhite[:, index+10:]
-cv2.imwrite("first.jpeg" ,firstDigit)
-cv2.imwrite("second.jpeg" ,secondDigit)
+# cv2.imwrite("first.jpeg" ,firstDigit)
+# cv2.imwrite("second.jpeg" ,secondDigit)
+
+model = import_model()
+
+os.chdir(testpath)
+
+firstDigit = cv2.resize(firstDigit, (28, 28), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite("first_resize.jpeg", firstDigit)
+firstDigit = firstDigit.reshape(1, 28, 28)
+
+secondDigit = cv2.resize(secondDigit, (28, 28), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite("second_resize.jpeg", secondDigit)
+secondDigit = secondDigit.reshape(1, 28, 28)
+
+first_result = numpy.argmax(model.predict(firstDigit)[0])
+second_result = numpy.argmax(model.predict(firstDigit)[0])
+
+print(first_result, second_result)
